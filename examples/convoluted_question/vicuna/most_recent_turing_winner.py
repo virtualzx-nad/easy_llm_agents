87b32ae6f6e4a19a953d84758a53fac9311daa96
Xiaolei Zhu <virtualzx@gmail.com>
2023-05-12 09:11:17 -0700

    feat: update examples

--- examples/convoluted_question/vicuna/most_recent_turing_winner.py
+++
@@ -1,7 +0,0 @@
-Here is a Python script that will print the most recent Turing award winner's name:
-```
-most_recent_turing_award_winner = "Bob Metcalfe"
-
-print(most_recent_turing_award_winner)
-```
-This script will output the string "Bob Metcalfe" as the most recent Turing award winner.
--- lm_agent/models/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,3 +0,0 @@
-from .model import CompletionModel
-from .openai import GPT35, GPT4
-from .fastchat import Vicuna, WizardLM
